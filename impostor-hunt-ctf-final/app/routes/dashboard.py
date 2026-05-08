6739f2f645a254c3c483966fde9604251820b0a3
DioJoestar <ezzeldinmarwan733@gmail.com>
2026-05-09 01:32:42 +0300

    Update dashboard.py

---
+++ impostor-hunt-ctf-final/app/routes/dashboard.py
@@ -0,0 +1,16 @@
+from flask import Blueprint,render_template
+from flask_login import current_user,login_required
+
+dashboard_bp = Blueprint('dashboard',__name__)
+
+@dashboard_bp.route('/dashboard')
+@login_required
+
+def index():
+    fixed_rooms = current_user.get_fixed_rooms()
+
+    progress_count = current_user.get_rooms_fixed_count()
+
+    all_done = current_user.check_all_rooms_fixed()
+
+    return render_template('main/dashboard.html',fixed_rooms=fixed_rooms,progress_count=progress_count,all_done=all_done)
